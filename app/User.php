8a1798afa4a28d9e521cd9ef016b15e0b1147066
Sergey2829 <sergey2829@gmail.com>
2020-01-30 17:45:29 +0200

    generate new model

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Psy\Util\Str;
 
 class User extends Authenticatable
 {
@@ -36,4 +37,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function questions()
+    {
+        $this->hasMany(Question::class);
+    }
+
+    public function setAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug('$value');
+
+    }
 }
